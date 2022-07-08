893f6fd0e4d3ce327ff4685335efaece6be8a836
nghkh <107782283+nghkh@users.noreply.github.com>
2022-07-08 10:32:43 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _checkBoxVal = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Column(
+          children: [
+            const ListTile(
+                leading: Text(
+                  'Task',
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                ),
+                trailing: Text('View All',
+                    style: TextStyle(fontSize: 18, color: Colors.blue))),
+            ListTile(
+              leading: Checkbox(
+                shape: const CircleBorder(),
+                onChanged: (bool? value) {
+                  if (value != null) {
+                    setState(() => _checkBoxVal = value);
+                  }
+                },
+                value: _checkBoxVal,
+              ),
+              title: const Text('Finishing Hi-Fi'),
+              trailing: const Icon(
+                Icons.drag_handle_rounded,
+                size: 30,
+              ),
+            ),
+            const Divider(),
+            ListTile(
+              leading: Checkbox(
+                shape: const CircleBorder(),
+                onChanged: (bool? value) {
+                  if (value != null) {
+                    setState(() => _checkBoxVal = value);
+                  }
+                },
+                value: _checkBoxVal,
+              ),
+              title: const Text('Meeting with Joe'),
+              trailing: const Icon(
+                Icons.drag_handle_rounded,
+                size: 30,
+              ),
+            ),
+            const Divider(),
+            ListTile(
+              leading: Checkbox(
+                shape: const CircleBorder(),
+                onChanged: (bool? value) {
+                  if (value != null) {
+                    setState(() => this._checkBoxVal = value);
+                  }
+                },
+                value: this._checkBoxVal,
+              ),
+              title: const Text('Finishing wireframe',
+                  style: TextStyle(
+                    decoration: TextDecoration.lineThrough,
+                    color: Colors.grey)),
+              trailing: const Icon(
+                Icons.drag_handle_rounded,
+                size: 30,
+              ),
+            ),
+            const Divider(),
+            ListTile(              leading: Checkbox(
+                shape: const CircleBorder(),
+                onChanged: (bool? value) {
+                  if (value != null) {
+                    setState(() => _checkBoxVal = value);
+                  }
+                },
+                value: _checkBoxVal,
+              ),
+              title: const Text('Sending invoice',
+                  style: TextStyle(
+                    decoration: TextDecoration.lineThrough,
+                    color: Colors.grey)),
+              trailing: const Icon(
+                Icons.drag_handle_rounded,
+                size: 30,
+              ),),
+          ],
+        ));
+  }
+}
